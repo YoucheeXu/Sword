340fbc7479cc598664f3d40b1c681b3e72cf38a0
Youchee <youchi.xu@live.com>
2026-05-16 08:19:44 +0800

    feat: add smart pointer alias name

--- Sword/src/Sword/Core/Core.h
+++ Sword/src/Sword/Core/Core.h
@@ -1,6 +1,8 @@
 #ifndef B33512D9_1515_4CD8_9044_84E3EBE08700
 #define B33512D9_1515_4CD8_9044_84E3EBE08700
 
+#include <memory>
+
 #ifdef SW_PLATFORM_WINDOWS
 #    ifdef SW_BUILD_DLL
 #        define SWORD_API __declspec(dllexport)
@@ -34,5 +36,17 @@
 #define BIT(x) (1 << x)
 
 #define SW_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Sword {
+
+    template<typename T>
+    using Scope = std::unique_ptr<T>;
+
+
+    template<typename T>
+    using Ref = std::shared_ptr<T>;
+
+}
+
 
 #endif /* B33512D9_1515_4CD8_9044_84E3EBE08700 */
